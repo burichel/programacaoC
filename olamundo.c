f4288443bb751289d4ce8ffdf8e5c2b66bf3f15e
burichel <deivson.burichel@gmail.com>
2025-01-29 23:09:31 +0000

    1

--- olamundo.c
+++ olamundo.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
+
+int main() {
+
+    char Estado [20] = "indefinido";
+    char Código [20] = "indefinido";
+    char Cidade [20] = "indefinido";
+    float População;
+    float Área;
+    float Pib;
+    int Pontos Turísticos;
+    char sim ='s';
+
+    // APRESENTAÇÃO DO PROGRAMA + INSTRUÇOES DE USABILIDADE
+
+    printf("*****     INTRODUÇÃO     *****\n");
+
+    
+}
 
